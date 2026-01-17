2d7d706066f983a9bdc93c7f81cbd7e10fa08df7
Rishabh Dagli <168488595+rishabhdagli@users.noreply.github.com>
2026-01-16 19:26:01 -0500

    Add Settings page with placeholder content

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,1 +1,11 @@
+import Layout from '../components/Layout';
+
+export default function Settings() {
+  return (
+    <Layout>
+      <h1>Privacy & Account Settings (Placeholder)</h1>
+    </Layout>
+  );
+}
+
 
