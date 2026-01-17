7984bd856ed928d120f9bda1bd26b3b9b72642e8
Rishabh Dagli <168488595+rishabhdagli@users.noreply.github.com>
2026-01-16 19:25:11 -0500

    Update sessions.tsx

--- pages/sessions.tsx
+++ pages/sessions.tsx
@@ -1,1 +1,10 @@
+import Layout from '../components/Layout';
+
+export default function Sessions() {
+  return (
+    <Layout>
+      <h1>Session Breakdown (Placeholder)</h1>
+    </Layout>
+  );
+}
 
