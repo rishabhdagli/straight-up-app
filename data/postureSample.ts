466894d09bf7a933f2d018c2426ad56b6309c265
Rishabh Dagli <168488595+rishabhdagli@users.noreply.github.com>
2026-01-16 19:37:22 -0500

    Add sessionData and trendData for posture analysis

---
+++ data/postureSample.ts
@@ -0,0 +1,12 @@
+export const sessionData = [
+  { name: 'Neutral', value: 72 },
+  { name: 'Slouch', value: 28 },
+];
+
+export const trendData = [
+  { day: 'Mon', score: 80 },
+  { day: 'Tue', score: 82 },
+  { day: 'Wed', score: 78 },
+  { day: 'Thu', score: 85 },
+  { day: 'Fri', score: 83 },
+];
